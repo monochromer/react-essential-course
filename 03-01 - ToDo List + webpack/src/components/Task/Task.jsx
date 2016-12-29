924187149cddebac238dd5d43a8f4988888b9246
Monochromer <buzmakov@fdslab.ru>
2016-12-29 11:27:30 +0500

    правки для ToDoList

--- 03-01 - ToDo List + webpack/src/components/Task/Task.jsx
+++ 03-01 - ToDo List + webpack/src/components/Task/Task.jsx
@@ -4,20 +4,20 @@ import './Task.less';
 
 
 const Task = (props) => {
-  return (
-    <div className={'Task' + (props.completed ? ' Task--Completed' : '')}>
-        <div className='Task-StatusIcon'
-            title={props.completed ? 'Mark Task as Uncompleted' : 'Mark Task as Completed'}
-            onClick={props.onToDoStatusChanged}
-        />
-        <div className='Task-Text'>{props.children}</div>
-        <button className='Task-Delete'
-            title="Delete Task"
-            type='button'
-            onClick={props.onToDoDelete}
-        />
-    </div>
-  )
+    return(
+        <div className={'Task' + (props.completed ? ' Task--Completed' : '')}>
+            <div className='Task-StatusIcon'
+                title={props.completed ? 'Mark Task as Uncompleted' : 'Mark Task as Completed'}
+                onClick={props.onToDoStatusChanged}
+            />
+            <div className='Task-Text'>{props.children}</div>
+            <button className='Task-Delete'
+                title="Delete Task"
+                type='button'
+                onClick={props.onToDoDelete}
+            />
+        </div>
+        )
 };
 
 
--- 03-01 - ToDo List + webpack/src/components/ToDoList/ToDoList.jsx
+++ 03-01 - ToDo List + webpack/src/components/ToDoList/ToDoList.jsx
@@ -4,27 +4,52 @@ import Task from '../Task/Task.jsx';
 
 import './ToDoList.less';
 
+class ToDoListItem extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    shouldComponentUpdate(nextProps, nexState) {
+        var cond = nextProps.task.completed !== this.props.completed;
+        return cond;
+    }
+    
+    render() {
+        var props = this.props;
+        return (
+            <li className='ToDoList-Item'>
+                <Task
+                    completed={props.task.completed}
+                    onToDoDelete={props.deleteCallback.bind(null, props.task)}
+                    onToDoStatusChanged={props.changedCallback.bind(null, props.task)}
+                >
+                    {props.task.text}
+                </Task>
+            </li>
+        )
+    }
+};
+
 
 const ToDoList = (props) => {
   var changedCallback = props.onToDoStatusChanged;
   var deleteCallback = props.onToDoDelete;
 
-  const tasks = props.tasks.map((task, index) => {
+  const tasks = props.tasks.map((task) => {
     return (
-      <li key={task.id} className='ToDoList-Item'>
-          <Task
-              completed={task.completed}
-              onToDoDelete={deleteCallback.bind(null, task)}
-              onToDoStatusChanged={changedCallback.bind(null, task)}
-          >
-              {task.text}
-          </Task>
-      </li>
+        <ToDoListItem
+            key={task.id}
+            task={task}
+            changedCallback={changedCallback}
+            deleteCallback={deleteCallback}
+        />
     )
   });
 
   return (
-    <ul className='ToDoList'>{tasks}</ul>
+    <ul className='ToDoList'>
+        {tasks}
+    </ul>
   )
 };
 
